80fe9020d3654e2c18e7335f237128d26f076113
Oldschool69 <flavio.marcondes@gmail.com>
2025-06-26 22:29:53 -0300

    Adicionados comentários no código

--- greed_melhores_estacoes_radio.py
+++ greed_melhores_estacoes_radio.py
@@ -1,3 +1,6 @@
+# Utilização de algoritmo guloso utilizando set 
+# para encontrar o conjunto de estações de rádio
+# que abrangem todos os estados  
 
 estacoes = {}
 
@@ -12,19 +15,24 @@ def popular_estacoes():
 def localizar_melhores_estacoes():
     popular_estacoes()
     estados_abranger = set(["mt", "wa", "or", "id", "nv", "ut", "ca", "az"])
-    estacoes_finais = set()
+    estacoes_finais = set() # no set vc remove entradas duplicadas
     
     while estados_abranger:
         melhor_estacao = None
         estados_cobertos = set()
         
         for estacao, estados in estacoes.items():
-            cobertos = estados_abranger & estados
+            # usando o operador & retorna a intersecção de valores comuns
+            # entre 2 sets 
+            cobertos = estados_abranger & estados 
             if(len(cobertos) > len(estados_cobertos)):
                 melhor_estacao = estacao
                 estados_cobertos = cobertos
-                
+        
+        # atualiza o set de estados a abranger removendo os estados ja cobertos                
         estados_abranger -= estados_cobertos
+        
+        # armazena a melhor estacao encontrada
         estacoes_finais.add(melhor_estacao)
     
     print("estacoes finais ", estacoes_finais)
